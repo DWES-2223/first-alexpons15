212f01a8f5a49ef359934cd55acb8e3e59f1a9ed
Alejandro Pons <aleponroc@alu.edu.gva.es>
2023-10-02 14:15:25 +0200

    [ADD] Activitat 270

---
+++ php/src/270.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <title>Activitat 270 - Rècords d'atletisme</title>
+</head>
+<body>
+<h1>Rècords registrats d'atletisme</h1>
+<?php include_once('270a.php')?>
+</body>
+</html>
