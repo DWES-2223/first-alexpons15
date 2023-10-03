ec348398065f3ebf3b834fdfb5f1e2194180706e
Alejandro Pons <aleponroc@alu.edu.gva.es>
2023-10-03 19:40:48 +0200

    [ADD] Exercicis 302 i 303

---
+++ php/src/Persona8.php
@@ -0,0 +1,45 @@
+<?php
+class Persona8 {
+    public function __construct(
+        private string $nom,
+        private string $cognoms,
+        private int $edat = 25) {}
+
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): void
+    {
+        $this->nom = $nom;
+    }
+
+    public function getCognoms(): string
+    {
+        return $this->cognoms;
+    }
+
+    public function setCognoms(string $cognoms): void
+    {
+        $this->cognoms = $cognoms;
+    }
+
+    public function getEdat(): int
+    {
+        return $this->edat;
+    }
+
+    public function setEdat(int $edat): void
+    {
+        $this->edat = $edat;
+    }
+
+    public function getNombreCompleto(): string {
+        return $this->nom . ' ' . $this->cognoms;
+    }
+    
+    public function estaJubilado(): bool {
+        return $this->edat >= 67;
+    }
+}
